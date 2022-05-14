ba7025739d5815030a17dca88f26b8881894ad79
Elaine Danahy <edanahy22@gmail.com>
2022-05-14 10:17:40 -0500

    modal

---
+++ assets/js/materialize.js
@@ -0,0 +1,14 @@
+// modal trigger
+$(document).ready(function(){
+    $('.modal').modal();
+  });
+          
+// form selection options
+  $(document).ready(function(){
+    $('select').formSelect();
+  }); 
+
+// form text-input(zipcode)
+$(document).ready(function() {
+    M.updateTextFields();
+  });
