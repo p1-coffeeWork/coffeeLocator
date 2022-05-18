2d6306ee6568cf48e2527bd7c909134030a2862e
Anthony Poku <tonypoku@gmail.com>
2022-05-17 21:12:04 -0500

    activated geo location

--- assets/js/yelp.js
+++ assets/js/yelp.js
@@ -21,76 +21,76 @@ if ("geolocation" in navigator) {
   }
 
   // requesting permission to get location info
-  // navigator.geolocation.getCurrentPosition(
-  //   // will be called if user allows location info
-  //   function (position) {
-  //     maplink.addEventListener("click", function (event) {
-  //       event.preventDefault();
-  //       console.log(event);
+  navigator.geolocation.getCurrentPosition(
+    // will be called if user allows location info
+    function (position) {
+      maplink.addEventListener("click", function (event) {
+        event.preventDefault();
+        console.log(event);
 
-  //       var queryParams = new URLSearchParams({
-  //         term: "coffee",
-  //         latitude: position.coords.latitude,
-  //         longitude: position.coords.longitude,
-  //       });
-  //       fetch(`${cors_anywhere_url}/${yelp_search_url}?${queryParams}`, {
-  //         headers: { Authorization: token },
-  //       })
-  //         .then(function (response) {
-  //           return response.json();
-  //         })
-  //         .then(handleYelpData)
-  //         .catch(function (error) {
-  //           console.error(error);
-  //         });
-  //     });
-  //   },
-  //   // will be called if user denies or there is an error
-  //   function (error) {
-  //     // adding zip input for location
-  //     $(modalEl.querySelector(".modal-content")).append(`
-  //       <div class="input-field col s6">
-  //         <input id="zip_code" type="text" class="validate">
-  //         <label for="zip_code">Zip Code</label>
-  //       </div>
-  //     `);
+        var queryParams = new URLSearchParams({
+          term: "coffee",
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+        fetch(`${cors_anywhere_url}/${yelp_search_url}?${queryParams}`, {
+          headers: { Authorization: token },
+        })
+          .then(function (response) {
+            return response.json();
+          })
+          .then(handleYelpData)
+          .catch(function (error) {
+            console.error(error);
+          });
+      });
+    },
+    // will be called if user denies or there is an error
+    function (error) {
+      // adding zip input for location
+      $(modalEl.querySelector(".modal-content")).append(`
+        <div class="input-field col s6">
+          <input id="zip_code" type="text" class="validate">
+          <label for="zip_code">Zip Code</label>
+        </div>
+      `);
 
-  //     modalEl.addEventListener("submit", function (event) {
-  //       event.preventDefault();
+      modalEl.addEventListener("submit", function (event) {
+        event.preventDefault();
 
-  //       var zipCode = modalEl.querySelector("input#zip_code").value.trim();
-  //       var queryParams = new URLSearchParams({
-  //         term: "coffee",
-  //         location: zipCode,
-  //       });
-  //       fetch(`${cors_anywhere_url}/${yelp_search_url}?${queryParams}`, {
-  //         headers: { Authorization: token },
-  //       })
-  //         .then(function (response) {
-  //           return response.json();
-  //         })
-  //         .then(handleYelpData)
-  //         .catch(function (error) {
-  //           console.error(error);
-  //         });
-  //     });
-  //   }
-  // );
+        var zipCode = modalEl.querySelector("input#zip_code").value.trim();
+        var queryParams = new URLSearchParams({
+          term: "coffee",
+          location: zipCode,
+        });
+        fetch(`${cors_anywhere_url}/${yelp_search_url}?${queryParams}`, {
+          headers: { Authorization: token },
+        })
+          .then(function (response) {
+            return response.json();
+          })
+          .then(handleYelpData)
+          .catch(function (error) {
+            console.error(error);
+          });
+      });
+    }
+  );
 }
 
-function displayMap() {
-  var queryParams = new URLSearchParams({
-    term: "coffee",
-    latitude: 41.87396088943666,
-    longitude: -87.95070683026313,
-  });
-  fetch(`${cors_anywhere_url}/${yelp_search_url}?${queryParams}`, {
-    headers: { Authorization: token },
-  })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(handleYelpData);
-}
+// function displayMap() {
+//   var queryParams = new URLSearchParams({
+//     term: "coffee",
+//     latitude: 41.87396088943666,
+//     longitude: -87.95070683026313,
+//   });
+//   fetch(`${cors_anywhere_url}/${yelp_search_url}?${queryParams}`, {
+//     headers: { Authorization: token },
+//   })
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(handleYelpData);
+// }
 
-maplink.addEventListener("click", displayMap);
+// maplink.addEventListener("click", displayMap);
